ed6a211d720e7749da78f4f3a1eb49f45bbb796a
NicoleHerbas <hfn5001384@est.univalle.edu>
2025-04-07 16:46:50 -0400

    Datos de Base de Nicole, no critiquen la contraseña

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,14 @@
 const { Pool } = require('pg');
-const { Pool } = require('pg');
 
-// Configura tu conexión a PostgreSQL
 // Configura tu conexión a PostgreSQL
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'vuelos',
-  password: 'templario2233',
+  database: 'AirDB',
+  password: '1234',
   port: 5432, // puerto por defecto de PostgreSQL
 });
 
-// Verifica la conexión
-pool.connect()
-  .then(() => console.log('🟢 Conectado a PostgreSQL'))
-  .catch(err => console.error('🔴 Error al conectar a PostgreSQL:', err));
-
-// Exporta el pool para usar en otros archivos
 // Verifica la conexión
 pool.connect()
   .then(() => console.log('🟢 Conectado a PostgreSQL'))
