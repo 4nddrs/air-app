417a7c8e8b1a6ce8e52c146835f21057e9c8e92b
NicoleHerbas <hfn5001384@est.univalle.edu>
2025-04-07 15:17:41 -0400

    Eliminar consulta repetida

--- backend/server.js
+++ backend/server.js
@@ -223,24 +223,6 @@ app.get('/api/vuelos/origen-fecha', async (req, res) => {
 });
 
 //Buscar pasajero
-app.get("/api/pasajeros/buscar", async (req, res) => {
-  const { q } = req.query; // lo que el usuario está escribiendo
-
-  if (!q || q.length < 2) {
-    return res.json([]); // mínimo 2 caracteres
-  }
-
-  try {
-    const result = await pool.query(
-      "SELECT pasaporte, nombre FROM pasajeros WHERE pasaporte::text LIKE $1 LIMIT 10",
-      [`${q}%`]
-    );
-    res.json(result.rows);
-  } catch (err) {
-    console.error("❌ Error al buscar pasajeros:", err);
-    res.status(500).send("Error del servidor");
-  }
-});
 app.get("/api/pasajeros/sugerencias", async (req, res) => {
   const { q } = req.query;
 
